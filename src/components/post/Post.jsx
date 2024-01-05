f516f161967aa53cffe44b6241657135b2347bf1
ktjn1129 <97653478+ktjn1129@users.noreply.github.com>
2024-01-05 21:41:55 +0900

    add API calling

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Post.css";
 import { MoreVert } from "@mui/icons-material";
-import { Users } from "../../dummyData";
 import { useState } from "react";
+import axios from "axios";
+import { format } from "timeago.js";
+import { Link } from "react-router-dom";
 
 export default function Post({ post }) {
   const PUBLIC_FOLDER = process.env.REACT_APP_PUBLIC_FOLDER;
-  const user = Users.filter((user) => user.id === post.userId)[0];
 
-  const [like, setLike] = useState(post.like);
+  const [like, setLike] = useState(post.likes.length);
   const [isLiked, setIsLiked] = useState(false);
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await axios.get(`/users?userId=${post.userId}`);
+      setUser(response.data);
+    };
+    fetchUser();
+  }, [post.userId]);
 
   const handleLike = () => {
     setLike(isLiked ? like - 1 : like + 1);
@@ -21,13 +31,17 @@ export default function Post({ post }) {
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img
-              src={PUBLIC_FOLDER + user.profilePicture}
-              alt=""
-              className="postProfileImg"
-            />
+            <Link to={`/profile/${user.username}`}>
+              <img
+                src={
+                  user.profilePicture || PUBLIC_FOLDER + "/person/noAvatar.png"
+                }
+                alt=""
+                className="postProfileImg"
+              />
+            </Link>
             <span className="postUsername">{user.username}</span>
-            <span className="postDate">{post.date}</span>
+            <span className="postDate">{format(post.createdAt)}</span>
           </div>
           <div className="postTopRight">
             <MoreVert />
@@ -35,7 +49,7 @@ export default function Post({ post }) {
         </div>
         <div className="postCenter">
           <span className="postText">{post.desc}</span>
-          <img src={PUBLIC_FOLDER + post.photo} alt="" className="postImg" />
+          <img src={PUBLIC_FOLDER + post.img} alt="" className="postImg" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
--- src/components/timline/Timeline.jsx
+++ src/components/timline/Timeline.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Timeline.css";
 import Share from "../share/Share";
 import Post from "../post/Post";
-import { Posts } from "../../dummyData.js";
+import axios from "axios";
 
-export default function Timeline() {
+export default function Timeline({ username }) {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = username
+        ? await axios.get(`/posts/profile/${username}`)
+        : await axios.get("/posts/timeline/6585821a3efa9ce469c4b83d");
+      setPosts(response.data);
+    };
+    fetchPosts();
+  }, [username]);
+
   return (
     <div className="timeline">
       <div className="timelineWrapper">
         <Share />
-        {Posts.map((post) => (
-          <Post post={post} key={post.id} />
+        {posts.map((post) => (
+          <Post post={post} key={post._id} />
         ))}
       </div>
     </div>
